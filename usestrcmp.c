8920283d1fbd525903aa7ab431968f788f7601c7
Jason Juhyun Kim <mtinet@hanmail.net>
2017-04-05 15:58:49 +0900

    Create usestrcmp.c

---
+++ usestrcmp.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char inputOne[5] = "a";
+	char inputTwo[5] = "a";
+	printf("문자열비교 : %d\n", strcmp(inputOne, inputTwo));
+	return 0;
+	
+}
